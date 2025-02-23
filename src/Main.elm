7e7c0b3fa6f43041fc07b7eaa159ff0bbc7fa93a
Jess Renteria <jessrenteria@users.noreply.github.com>
2025-02-23 13:01:28 -0800

    Add skeleton Main.elm.

---
+++ src/Main.elm
@@ -0,0 +1,28 @@
+module Main exposing (..)
+
+import Browser
+import Element exposing (..)
+import Html exposing (Html)
+
+main : Program () Model Msg
+main = Browser.sandbox { init = init, view = view, update = update }
+
+
+type alias Model =
+    { message : String
+    }
+
+
+init : Model
+init = { message = "Countdown!" }
+
+
+type alias Msg = ()
+
+
+update : Msg -> Model -> Model
+update _ model = model
+
+
+view : Model -> Html Msg
+view model = layout [] <| el [] <| text model.message
